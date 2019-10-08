871b9aac48df81ef3808845cb36d3923cf506286
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-08 14:11:19 +0100

    set up state in Component and rendered todo list

--- reducer-todo/src/Reducer/Reducer.js
+++ reducer-todo/src/Reducer/Reducer.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const todoList = [
+export const todoList = [
   { item: "You Dont Know JS", completed: false, id: 3892987589 },
   { item: "Front End Masters", completed: false, id: 3892987555 },
   { item: "Redux", completed: false, id: 3892987589 }
 ];
+
+
+
+
 
 export default function reducer(state, action) {
   switch (action.type) {
